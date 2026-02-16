42bd025c03434a717efcc3571b744c87411c0a4a
Richard Hull <rm_hull@yahoo.co.uk>
2026-02-16 17:54:41 +0000

    refactor: Deduplicate repeated fuel prices
    Updates the price search query to ensure that redundant
    price reports (where the price value is unchanged from
    a previous entry) are filtered out before calculating
    the overall recency rank.
    
    The new nested structure first filters based on the
    `same_price_rank` to keep only the most recent
    `price_last_updated` timestamp for any unique
    combination of `node_id`, `fuel_type`, and `price`.

--- internal/sql/search_prices.sql
+++ internal/sql/search_prices.sql
@@ -5,14 +5,23 @@ SELECT
   price
 FROM (
   SELECT
-    fp.node_id,
-    fp.fuel_type,
-    fp.price_last_updated,
-    fp.price,
-    ROW_NUMBER() OVER (PARTITION BY fp.node_id, fp.fuel_type ORDER BY fp.price_last_updated DESC) AS rn
-  FROM petrol_filling_stations pfs
-  INNER JOIN fuel_prices fp ON pfs.node_id = fp.node_id
-  WHERE pfs.latitude BETWEEN ? AND ?
-    AND pfs.longitude BETWEEN ? AND ?
-) t
-WHERE rn <= ?;
+    node_id,
+    fuel_type,
+    price_last_updated,
+    price,
+    ROW_NUMBER() OVER (PARTITION BY node_id, fuel_type ORDER BY price_last_updated DESC) AS price_recency_rank
+  FROM (
+    SELECT
+      fp.node_id,
+      fp.fuel_type,
+      fp.price_last_updated,
+      fp.price,
+      ROW_NUMBER() OVER (PARTITION BY fp.node_id, fp.fuel_type, fp.price ORDER BY fp.price_last_updated DESC) AS same_price_rank
+    FROM petrol_filling_stations pfs
+    INNER JOIN fuel_prices fp ON pfs.node_id = fp.node_id
+    WHERE pfs.latitude BETWEEN ? AND ?
+      AND pfs.longitude BETWEEN ? AND ?
+  ) t1
+  WHERE same_price_rank = 1
+) t2
+WHERE price_recency_rank <= ?;
