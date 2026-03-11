96761f91cdb44731161d6ea52f8d1d58a3c06396
Richard Hull <rm_hull@yahoo.co.uk>
2026-03-11 00:47:12 +0000

    fix: adjust price threshold in cleanse script
    Updated the `delete` condition to remove entries with prices less than
    110 to ensure data consistency in the `fuel_prices` table.

--- docs/cleanse.sql
+++ docs/cleanse.sql
@@ -8,5 +8,5 @@ delete from fuel_prices where price = 99.9;
 update fuel_prices set price = price * 100 where price >=1 and price <2;
 update fuel_prices set price = price * 10 where price >=12 and price <20;
 update fuel_prices set price = price + 100 where price >=20 and price <100;
-delete from fuel_prices where price < 100;
+delete from fuel_prices where price < 110;
 
